1dd46a3d5469f872ef7138ebc5737a2ff2484315
Pablo <cabezapablo01@gmail.com>
2022-03-04 16:32:22 +0100

    Hola mundo

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
-<?php
 
+<?php
+/*
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
@@ -7,3 +8,13 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+*/
+?>
+
+
+<html>
+    <body>
+        <h1> Hola mundo wadawdawdawdawdawdaw
+            awdawdd</h1>
+    </body>
+</html>
