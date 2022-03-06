e3f074be50ce0b05c59a3be88899f0f9c85a5653
haxezeta <aranda.formal@gmail.com>
2022-03-06 21:10:43 +0100

    Merge branch 'main' of https://github.com/Proyecto-ADIST/ADIST into main
    Co-authored-by: cabeezaa0 <cabeezaa0@users.noreply.github.com>

--- README.md
+++ README.md
@@ -33,3 +33,10 @@ Proyecto Grupal ADIST
 - Aranda Cañada, Antonio Jesús ► [haxezeta](https://github.com/haxezeta)
 - Cabeza Acal, Francisco José ► [Langdom91](https://github.com/Langdom91)
 - Cabeza Acal, Pablo ► [cabeezaa01](https://github.com/cabeezaa01)
+
+## Instalación del proyecto:
+
+1. Clonar el repositorio.
+2. Ejecutar ``composer install`` para instalar las dependencias software.
+3. Hacer una copia del archivo ``.env`` llamada ``.env.local`` y personalizar la configuración de la base de datos.
+4. Ejecutar ``php bin/console doctrine:migrations:migrate`` para ejecutar las migraciones
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -12,6 +12,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\CallbackTransformer;
 
 class RegistrationFormType extends AbstractType
 {
@@ -22,8 +23,13 @@ class RegistrationFormType extends AbstractType
             ->add('email')
             ->add('lastname')
             ->add('roles', ChoiceType::class, [
-                'choices' => [
-                    'Administrador' => array("ROLE_ADMIN")
+                'required' => true,
+                'multiple' => false,
+                'expanded' => false,
+                'choices'  => [
+                  'Administrador' => 'ROLE_ADMIN',
+                  'Repartidor' => 'ROLE_REPARTIDOR',
+                  'Almacenista' => 'ROLE_ALMACENISTA',
                 ],
             ])
             ->add('agreeTerms', CheckboxType::class, [
@@ -52,6 +58,19 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
         ;
+
+        // Data transformer
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                     // transform the array to a string
+                     return count($rolesArray)? $rolesArray[0]: null;
+                },
+                function ($rolesString) {
+                     // transform the string back to an array
+                     return [$rolesString];
+                }
+        ));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
