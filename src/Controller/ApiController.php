5e8f6c015322140044b3625c876fdbc276b9dad2
= <=>
2022-03-09 10:23:46 +0100

    ajax 3

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use App\Entity\Pedido;
 use App\Entity\Producto;
-use App\Repository\ProductoRepository;
 
 class ApiController extends AbstractController
 {
@@ -15,7 +14,7 @@ class ApiController extends AbstractController
         $entityManager = $this->getDoctrine()->getManager();
         $productos = $entityManager->getRepository(Producto::class)->findBySimilarName($request->request->get("nombre"));
         
-        
+        //crear array vacio llenarlo con los nombres de los productos y crear un jsonresponse con los productos
         return new JsonResponse($this->generateUrl('api_buscar_producto', [
             'id' => $productos->getId(),
           ], UrlGeneratorInterface::ABSOLUTE_URL), 201);
