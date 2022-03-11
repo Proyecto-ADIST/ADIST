dddbf316f888bbf6da49d5be424c2b1e5ed9d960
= <=>
2022-03-11 13:30:30 +0100

    js

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,26 +1,102 @@
 
-function obtenerProductoPorTipoProducto(id){
+function obtenerProductoPorTipoProducto(id) {
 
         var urlBusqueda = "/api/productos/" + id;
-        
-        $.ajax({                
-                url:   urlBusqueda,
-                type:  'get',
+
+        $.ajax({
+                url: urlBusqueda,
+                type: 'get',
                 beforeSend: function () {
                         $("#resultado").html("Procesando, espere por favor...");
                 },
-                success:  function (response) {
+                success: function (response) {
                         $("#resultado").html(response);
                         //pintarProducto();
                 }
         });
 
-        
-    }
 
-function pintarProducto(response){
+}
+
+function obtenerProducto(id) {
+
+        var urlBusqueda = "/api/productos/" + id;
+
+        $.ajax({
+                url: urlBusqueda,
+                type: 'get',
+                beforeSend: function () {
+                        $("#resultado").html("Procesando, espere por favor...");
+                },
+                success: function (response) {
+                        $("#resultado").html(response);
+                        //pintarProducto();
+                }
+        });
+
+
+}
+
+function borrarProducto(id) {
+
+        var urlBusqueda = "/api/productos/" + id;
+
+        $.ajax({
+                url: urlBusqueda,
+                type: 'delete',
+                beforeSend: function () {
+                        $("#resultado").html("Procesando, espere por favor...");
+                },
+                success: function (response) {
+                        $("#resultado").html(response);
+                        //pintarProducto();
+                }
+        });
+
+
+}
+
+function pintarProducto(response) {
 
         $response.id;
+
+
+}
+
+function obtenerTiendas() {
+
+        var urlBusqueda = "/api/tiendas/";
+
+        $.ajax({
+                url: urlBusqueda,
+                type: 'get',
+                beforeSend: function () {
+                        $("#resultado").html("Procesando, espere por favor...");
+                },
+                success: function (response) {
+                        $("#resultado").html(response);
+                        //pintarProducto();
+                }
+        });
+
+
+}
+
+function borrarTienda(id) {
+
+        var urlBusqueda = "/api/tiendas/" + id;
+
+        $.ajax({
+                url: urlBusqueda,
+                type: 'delete',
+                beforeSend: function () {
+                        $("#resultado").html("Procesando, espere por favor...");
+                },
+                success: function (response) {
+                        $("#resultado").html(response);
+                        //pintarProducto();
+                }
+        });
 
 
 }
