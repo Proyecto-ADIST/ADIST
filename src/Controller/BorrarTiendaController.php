f15e8644ac63b38ba84b005198b786d1d56f488b
cabeezaa0 <cabezapablo01@gmail.com>
2022-03-13 22:10:28 +0100

    Frontend tiendas

---
+++ src/Controller/BorrarTiendaController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Tienda;
+
+
+class BorrarTiendaController extends AbstractController
+{
+
+    public function index(): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $tiendas = $entityManager->getRepository(Tienda::class)->findAll();
+
+        return $this->render('componentesHTML/borrarTienda.html.twig', [
+            'controller_name' => 'BorrarTiendaController',
+            'tiendas' => $tiendas
+        ]);
+        
+    }
+}
---
+++ src/Controller/RegistroTiendaController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class RegistroTiendaController extends AbstractController
+{
+
+    public function index(): Response
+    {
+        return $this->render('componentesHTML/añadirTienda.html.twig', [
+            'controller_name' => 'RegistroTiendaController',  
+        ]);
+    }
+}
